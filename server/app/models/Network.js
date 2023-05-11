1a5a5100b06004cbc6dc99d14cc9f4d7225cb38e
Pedro Bonett <gilbonett@gmail.com>
2023-05-11 19:14:45 -0300

    create model for the entity Network

---
+++ server/app/models/Network.js
@@ -0,0 +1,41 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Network extends Model {
+    
+    static associate(models) {
+      
+    }
+  }
+  Network.init({
+    candidate_id: {
+      allowNull: false,
+      type: DataTypes.INTEGER,
+      references: {
+         model: 'Candidate',
+          key: 'id' 
+        },
+      onUpdate: 'CASCADE',
+      onDelete: 'CASCADE'
+    },
+    github: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    linkedin: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    portfolio: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+  }, {
+    sequelize,
+    modelName: 'Network',
+    freezeTableName: true
+  });
+  return Network;
+};
