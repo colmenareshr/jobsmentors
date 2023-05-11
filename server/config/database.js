12fe39036805ea30bd857a3987edd8d4ee75ac15
Pedro Bonett <gilbonett@gmail.com>
2023-05-11 18:30:35 -0300

    connected the models with the database

--- server/config/database.js
+++ server/config/database.js
@@ -1,12 +1,18 @@
 require('dotenv').config();
 
 module.exports= { 
-  username: process.env.DB_USERNAME || "root",
-  password: process.env.DB_PASSWORD || "110910",
-  database: process.env.DB_DATABASE || "database_development",
-  host: process.env.DB_HOST || "127.0.0.1",
-  dialect: process.env.DB_DIALECT || "mysql",
-  port : process.env.DB_PORT || 3306,    
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST, 
+  dialect: process.env.DB_DIALECT, 
+  port : process.env.DB_PORT, 
+  
+  
+  seederStorage: "sequelize",
+  seederStorageTableName: "seeds",
 
+  migrationStorage: "sequelize",
+  migrationStorageTableName: "migrations",
 }
 
