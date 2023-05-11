66a30fa72478ac15eb6492eea1e10aa6929e2632
Pedro Bonett <gilbonett@gmail.com>
2023-05-11 17:17:57 -0300

    using package to protect variables

--- server/config/database.js
+++ server/config/database.js
@@ -1,8 +1,12 @@
-module.exports = {
-    username:  "root",
-    password:  "110910",
-    database:  "jobsmentors_development",
-    host: "127.0.0.1",
-    dialect:  "mysql",
-    port :  3306
+require('dotenv').config();
+
+module.exports= { 
+  username: process.env.DB_USERNAME || "root",
+  password: process.env.DB_PASSWORD || "110910",
+  database: process.env.DB_DATABASE || "database_development",
+  host: process.env.DB_HOST || "127.0.0.1",
+  dialect: process.env.DB_DIALECT || "mysql",
+  port : process.env.DB_PORT || 3306,    
+
 }
+
