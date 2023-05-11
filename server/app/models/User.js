f4d4be80f783de1c71b0c331c45d2cc717426c28
Pedro Bonett <gilbonett@gmail.com>
2023-05-11 18:34:51 -0300

    create model for the entity user

---
+++ server/app/models/User.js
@@ -0,0 +1,38 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    
+    static associate(models) {
+     
+    }
+  }
+  User.init({
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    email: {
+      allowNull: false,
+      unique: true,
+      type: DataTypes.STRING
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    role: {
+      allowNull: false,
+      type:DataTypes.ENUM('candidate', 'company'),
+    },
+  }, {
+    sequelize,
+    modelName: 'User',
+    freezeTableName: true
+  });
+  return User;
+};
