df5be8d2b7757667646608eb399e81cb3250662f
José Freites <jlfu2004@gmail.com>
2023-05-13 11:34:14 -0300

    ADDED login/sign buttons to navbar

--- client/src/components/Navbar/Navbar.tsx
+++ client/src/components/Navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import SignUpPage from 'components/SignUpPage/SignUpPage'
 import React from 'react'
 
 // modificación luego de eliminar branch jjose en gh
@@ -55,7 +56,7 @@ function Navbar() {
                 href="#"
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
-                About
+                Freelancer
               </a>
             </li>
             <li>
@@ -63,7 +64,7 @@ function Navbar() {
                 href="#"
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
-                Services
+                Company
               </a>
             </li>
             <li>
@@ -71,7 +72,7 @@ function Navbar() {
                 href="#"
                 className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
               >
-                Pricing
+                About Us
               </a>
             </li>
             <li>
@@ -81,6 +82,19 @@ function Navbar() {
               >
                 Contact
               </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+              >
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-1 px-4 rounded">
+                  Sign In
+                </button>
+              </a>
+            </li>
+            <li>
+              <SignUpPage />
             </li>
           </ul>
         </div>
