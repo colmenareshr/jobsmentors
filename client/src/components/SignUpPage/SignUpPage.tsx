f2592067f885c1f9cca5127e7c5d460b185b8c87
José Freites <jlfu2004@gmail.com>
2023-05-13 11:35:09 -0300

    Created compoonent SignUpPage

---
+++ client/src/components/SignUpPage/SignUpPage.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function SignUpPage() {
+  return (
+    <div>
+      <a
+        href="#"
+        className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+      >
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold p-1 px-4 rounded">
+          Login
+        </button>
+      </a>
+    </div>
+  )
+}
+
+export default SignUpPage
