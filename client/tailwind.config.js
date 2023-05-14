40e26f55fb76f94f6a000c99ff75d1d04cec3b31
Humberto Colmenares <colmenareshr@gmail.com>
2023-05-14 07:14:55 -0300

    add colors and fonts to tailwindcss config

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,6 +3,26 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      white: '#f4f4f4',
+      black: '#181818',
+      teal: '#001219',
+      teal400: '#0A9396',
+      sky: '#94D2BD',
+      orange: '#CA6702',
+      yellow: '#EE9B00'
+    },
+    fontFamily: {
+      sans: ['Poppins', 'sans-serif']
+    },
+    fontSize: {
+      sm: '.8rem',
+      base: '1rem',
+      lg: '1.2rem',
+      xl: '1.4rem',
+      '2xl': '1.6rem',
+      '3xl': '1.8rem'
+    },
     extend: {}
   },
   plugins: []
