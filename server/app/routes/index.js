74bd204e87a3b0223c588c3cf3d006e6cf0d2ab5
Pedro Bonett <gilbonett@gmail.com>
2023-05-14 16:07:08 -0300

    create Candidate Controller

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -1,10 +1,12 @@
 const bodyParser = require ('body-parser')
 const user = require('./usersRoute')
+const candidate = require('./candidateRoute')
 
 
 module.exports = app => {
     app.use(
         bodyParser.json(),
-        user
+        user,
+        candidate
     )
 }
