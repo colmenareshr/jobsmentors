330948a733f856d309f95bc4b1dc66b4bcfc8dc8
Humberto Colmenares <colmenareshr@gmail.com>
2023-05-16 00:22:16 -0300

    Edit herosection

--- client/src/components/HeroSection/index.tsx
+++ client/src/components/HeroSection/index.tsx
@@ -1,8 +1,8 @@
 const HeroSection = () => {
   return (
-    <section className="mx-auto flex  h-screen flex-col items-center justify-center gap-4 text-center md:mx-auto md:w-[600px]">
+    <section className="mx-auto flex h-screen flex-col items-center justify-center gap-4 text-center md:mx-auto md:w-[600px] md:pt-40">
       <h1 className="md:text-6xl">
-        Find the perfect freelance talent for your technological projects
+        Find the perfect freelance talent for your technological projects.
       </h1>
       <a href="#" className="button uppercase">
         Get a solution now
