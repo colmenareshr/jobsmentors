2292859ade221cb7da5e5d4907e9f5ad6c7b05bd
Humberto Colmenares <colmenareshr@gmail.com>
2023-05-16 00:24:11 -0300

    feat: Add first CTA to home page

---
+++ client/src/components/Main/index.tsx
@@ -0,0 +1,10 @@
+import CtaJuniors from 'components/CtaJuniors'
+function Main() {
+  return (
+    <main className="container mx-auto mt-[-265px] h-full max-w-full bg-emerald pt-36 text-center md:mt-[-170px] md:flex md:items-center md:justify-between md:py-2 md:text-left">
+      <CtaJuniors />
+    </main>
+  )
+}
+
+export default Main
