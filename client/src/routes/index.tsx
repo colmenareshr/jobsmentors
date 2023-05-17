b98ab9fdad00bb91c1468f7f1361137e4d1ee630
José Freites <jlfu2004@gmail.com>
2023-05-17 01:34:26 -0300

    ADDED companies route

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,10 +1,13 @@
 import Home from 'pages/Home'
 import { Routes, Route, Navigate } from 'react-router-dom'
+import Companies from '../components/Companies/Companies'
+
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="*" element={<Navigate to="/" />} />
+      <Route path="/companies" element={<Companies />} />
     </Routes>
   )
 }
