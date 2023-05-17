43bcec6d1b1620d0384964dbb3554b656326c0dc
José Freites <jlfu2004@gmail.com>
2023-05-17 01:33:56 -0300

    Add SearchTerm to useContext & type ProjectsProps

--- client/src/context/useStore.tsx
+++ client/src/context/useStore.tsx
@@ -1,13 +1,24 @@
 import { useEffect, useState } from 'react'
 
+export type ProjectsProps = {
+  id: number
+  title: string
+  description: string
+  skills: string[]
+  availability: number
+}
+
 export const useStore = () => {
   const [isOpenModalLogin, setIsOpenModalLogin] = useState(false)
   const [isOpenModalSign, setIsOpenModalSign] = useState(false)
+  const [searchTerm, setSearchTerm] = useState('')
 
   return {
     isOpenModalLogin,
     setIsOpenModalLogin,
     isOpenModalSign,
-    setIsOpenModalSign
+    setIsOpenModalSign,
+    searchTerm,
+    setSearchTerm
   }
 }
