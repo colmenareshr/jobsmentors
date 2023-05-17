29bf19c1bb068af2a0c62fec0d4af9b1b3558070
José Freites <jlfu2004@gmail.com>
2023-05-17 01:27:21 -0300

    ADDED link to home page

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,9 +1,12 @@
 import Navbar from 'components/Navbar/Navbar'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
     <header className="container mx-auto flex h-24 items-center justify-between border-b-2 border-b-sky/50 ">
-      <span className="px-3 text-xl font-bold md:text-2xl">JobsMentors</span>
+      <Link to="/" className="px-3 text-xl font-bold md:text-2xl">
+        JobsMentors
+      </Link>
       <Navbar />
     </header>
   )
