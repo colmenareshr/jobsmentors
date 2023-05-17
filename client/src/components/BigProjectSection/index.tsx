bb657194ee1bd75647304a03fc28fd777b497b91
Humberto Colmenares <colmenareshr@gmail.com>
2023-05-17 17:32:28 -0300

    edit for mobile version

--- client/src/components/BigProjectSection/index.tsx
+++ client/src/components/BigProjectSection/index.tsx
@@ -4,8 +4,8 @@ const BigProjectSection = () => {
   return (
     <section className="container mx-auto max-w-full bg-[#fff] py-14 text-center">
       <h2>Looking for a Development Team for Large-Scale Projects?</h2>
-      <div className="flex flex-col gap-4 py-5 md:flex-row md:items-center md:justify-center md:px-5">
-        <div className="w-[50%] text-left">
+      <div className="flex flex-col-reverse gap-4 py-5 md:flex-row md:items-center md:justify-center md:px-5">
+        <div className="md:w-[50%] md:text-left">
           <h3>Unlock the Potential of Your Projects</h3>
           <p className="pb-4">
             Build your dream team of skilled developers and elevate your
