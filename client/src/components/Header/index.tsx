1d0b2974416fc0e635c9a47f9539aa5ac552edb8
Humberto Colmenares <colmenareshr@gmail.com>
2023-05-18 18:55:55 -0300

    Cleaning code and add Freelancers page

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,9 +1,12 @@
 import Navbar from 'components/Navbar/Navbar'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
     <header className="container mx-auto flex h-24 items-center justify-between border-b-2 border-b-sky/50 ">
-      <span className="px-3 text-xl font-bold md:text-2xl">JobsMentors</span>
+      <span className="px-3 text-xl font-bold md:text-2xl">
+        <Link to="/">JobsMentors</Link>
+      </span>
       <Navbar />
     </header>
   )
--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,9 +1,11 @@
+import FreelancersPage from 'pages/Freelancers'
 import Home from 'pages/Home'
 import { Routes, Route, Navigate } from 'react-router-dom'
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="freelancers" element={<FreelancersPage />} />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   )
