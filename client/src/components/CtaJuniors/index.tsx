7c6a33912c90b58f49aee491b01fc07f0c6a5f14
Humberto Colmenares <colmenareshr@gmail.com>
2023-05-18 18:35:06 -0300

    editando la sección del avatar

--- client/src/components/CtaJuniors/index.tsx
+++ client/src/components/CtaJuniors/index.tsx
@@ -3,11 +3,11 @@ import contactFreelancer from '../../assets/images/contact-freelancers-jobsmento
 const CtaJuniors = () => {
   return (
     <section className="relative h-full w-full bg-emerald py-16 text-center">
-      <div className="absolute top-[-20%]  md:absolute md:-top-1/3  md:left-1/2 md:-translate-x-1/2 ">
-        <img src={Freelancer} alt="/" />
+      <div className="absolute top-[-20%] md:absolute md:-top-1/3  md:left-1/2 md:-translate-x-1/2 ">
+        <img className="z-9" src={Freelancer} alt="Freelancers Junior" />
       </div>
       <div className="container-lg mx-auto mt-24 grid md:mt-0 md:grid md:grid-cols-2 md:items-center md:justify-items-center md:px-9">
-        <div className="flex flex-col items-center gap-3 text-center text-white md:items-start md:text-left">
+        <div className="flex flex-col items-center gap-3 text-center text-white md:w-[80%] md:items-start md:text-left">
           <h2>
             Discover a skilled team to provide the solution your business needs.
           </h2>
