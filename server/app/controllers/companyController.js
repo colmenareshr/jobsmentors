0676d3f3de66ed5a00195e44ff029bec13aa3490
Pedro Bonett <gilbonett@gmail.com>
2023-05-19 19:14:33 -0300

    refactoring controllers and company routes

--- server/app/controllers/companyController.js
+++ server/app/controllers/companyController.js
@@ -1,7 +1,5 @@
 const database = require('../models')
 const Sequelize = require('sequelize');
-const Information = require('../models/Information');
-
 
 
 class CompanyController {
@@ -60,6 +58,35 @@ class CompanyController {
         }
     }
 
+    static async CreateJob(req, res) {
+        const job = req.body 
+        try {
+            const newJob = await database.Jobs.create(job)
+            return res.status(200).json(newJob)
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
+
+    static async findFreelancerSkills(req, res){
+        try {
+            const where = {}
+            const { hard_skills } = req.query;
+            if(hard_skills) where.hard_skills = { [Sequelize.Op.like]: `%${hard_skills}%` }
+            const information = await database.Freelancer.findAll( {
+                where: { 
+                    ...where
+                } 
+            });
+            if(information.length  === 0 ){
+                return res.status(400).send({message:'Information not found'})
+            } else{
+                return res.status(200).json(information)
+            }
+        } catch (error) {
+            return res.status(500).json(error.message)
+        }
+    }
 
 }
 
--- server/app/routes/companyRoute.js
+++ server/app/routes/companyRoute.js
@@ -1,11 +1,17 @@
 const {Router} = require ('express')
 const CompanyController = require('../controllers/companyController')
+const { authJwt } = require('../middlewares')
 
 const router = Router()
 
 router
-    .put('/company/profile/:id',  CompanyController.updateCompany)
-    .get('/user/company/:id', CompanyController.searchCompanyById)
-    .delete('/company/:id', CompanyController.deleteCompany)
     
+    .get('/company/:id', authJwt.verifyToken, CompanyController.searchCompanyById)
+    .get('/company/freelancerSkills', authJwt.verifyToken, authJwt.isCompany, CompanyController.findFreelancerSkills)
+    .post('/company/job', authJwt.verifyToken, authJwt.isCompany, CompanyController.CreateJob)
+    .put('/company/update/:id', authJwt.verifyToken, authJwt.isCompany,  CompanyController.updateCompany)
+    .delete('/company/:id', authJwt.verifyToken, authJwt.isCompany, CompanyController.deleteCompany)
+    
+
+
 module.exports = router
