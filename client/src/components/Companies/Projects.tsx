7fdb3e22ad04d994de083124ec7f35c39c4fcd72
José Freites <jlfu2004@gmail.com>
2023-05-19 02:50:00 -0300

    Tuning css code

--- client/src/components/Companies/Projects.tsx
+++ client/src/components/Companies/Projects.tsx
@@ -4,77 +4,81 @@ function Projects() {
   return (
     <div className="container mx-auto bg-emerald">
       <section className="bg-teal400/40">
-        <form className="bg-black/20">
+        <form className=" bg-black/20 p-4">
           <main
-            className="bg-gray-300 grid h-[600px] grid-flow-col-dense 
-                      grid-cols-12 grid-rows-5 gap-4 text-center
+            className="bg-gray-300 grid h-[900px] grid-flow-col-dense grid-cols-12 grid-rows-6
+                      gap-4 border-2 border-white p-4 text-center
                       "
           >
-            <header className="col-span-12 row-span-1">
+            <header className="col-span-12 row-span-1 flex flex-wrap  content-center justify-center">
               <p className="p-title-Projects text-center text-5xl font-black">
-                Creación de Formulario
+                INGRESE LOS DATOS DE SU PROYECTO Y BUSQUE FREELANCERS
               </p>
             </header>
-            <div className="col-span-4 row-span-1 bg-teal400">
+
+            <div className="col-span-4 row-span-1 self-center">
               <label htmlFor="" className="text-lg">
                 Nombre del Proyecto
               </label>
               <input
-                className="rouded-lg w-full p-3"
+                className="w-full rounded-lg p-3"
                 placeholder="nombre del proyecto"
                 type="text"
               />
             </div>
-            <div className="col-span-4 row-span-1 bg-orange/40">
+            <div className="col-span-4 row-span-1 self-center rounded-lg">
               <label htmlFor="" className="text-lg">
                 Habilidades requeridas
               </label>
               <input
-                className="rouded-lg w-full p-3"
+                className="rouded-lg w-full rounded-lg p-3"
                 placeholder="Javascript, React, Node, etc."
                 type="text"
               />
             </div>
-            <div className="col-span-4 row-span-1 bg-orange/40">
+            <div className="col-span-4 row-span-1 self-center">
               <label htmlFor="" className="text-lg">
                 Número de frelancer para el proyecto
               </label>
               <input
-                className="rouded-lg w-full p-3"
+                className="rouded-lg w-full rounded-lg p-3"
                 placeholder="min ( 1 ) . . . max ( 20 )"
                 type="text"
               />
             </div>
-            <div className="col-span-4 row-span-3 bg-yellow">
-              <label htmlFor="" className="text-lg">
+            <div className="col-span-4 row-span-3 rounded-lg p-4">
+              <label
+                htmlFor=""
+                className="flex content-center justify-center pb-4 text-center text-lg"
+              >
                 Descripción
               </label>
               <textarea
-                className="rouded-lg w-full scroll-smooth p-3"
+                className="rouded-lg h-3/4 w-full scroll-smooth rounded-lg p-3"
                 placeholder="descripción del proyecto"
               />
             </div>
-            <div className="col-span-4 row-span-3 flex flex-col place-content-center gap-4  bg-sky">
-              <div className="bg-orange">
-                <button className="w-80 rounded-full bg-purple p-5">
+            <div className="col-span-4 row-span-3 flex flex-col place-content-center gap-16">
+              <div className="">
+                <button className="w-80 rounded-full bg-yellow p-5 text-lg font-semibold shadow-lg">
                   GUARDAR
                 </button>
               </div>
-              <div className="bg-orange">
-                <button className="rounded-full bg-purple p-5 pl-20 pr-20">
+              <div className="">
+                <button className="rounded-full bg-yellow p-5 pl-20 pr-20 text-lg font-semibold shadow-lg">
                   ELIMINAR PROYECTO
                 </button>
               </div>
             </div>
-            <div className="col-span-12 flex w-full items-center justify-center bg-teal400/40">
+            <div className="col-span-12 row-span-2 flex w-full items-center justify-center bg-teal400/40">
               TABLA
             </div>
           </main>
         </form>
       </section>
       <section>
-        <div className="flex w-full flex-wrap justify-center bg-orange">
-          <button className="flex rounded-full bg-purple p-5 pl-20 pr-20">
+        <div className="flex w-full flex-wrap justify-center p-4">
+          <button className="flex rounded-full bg-yellow p-5 pl-20 pr-20 font-semibold shadow-lg">
             BUSCAR FREELANCERS
           </button>
         </div>
