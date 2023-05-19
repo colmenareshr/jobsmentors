9baf637425dd8ea16766966ff898f1901c5e6eae
José Freites <jlfu2004@gmail.com>
2023-05-19 02:03:55 -0300

    ADDED route for Projects component

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,6 +1,7 @@
 import Home from 'pages/Home'
 import { Routes, Route, Navigate } from 'react-router-dom'
 import Companies from '../components/Companies/Companies'
+import Projects from 'components/Companies/Projects'
 
 export const AppRoutes = () => {
   return (
@@ -8,6 +9,7 @@ export const AppRoutes = () => {
       <Route path="/" element={<Home />} />
       <Route path="*" element={<Navigate to="/" />} />
       <Route path="/companies" element={<Companies />} />
+      <Route path="/companies/projects" element={<Projects />} />
     </Routes>
   )
 }
