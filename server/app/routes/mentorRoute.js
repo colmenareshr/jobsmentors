a94fddd517db4fd21d10e1b6a7ebc5ab6311db38
Pedro Bonett <gilbonett@gmail.com>
2023-05-19 19:02:22 -0300

    creating mentor controllers

---
+++ server/app/routes/mentorRoute.js
@@ -0,0 +1,13 @@
+const {Router} = require ('express')
+const MentorController = require('../controllers/mentorController')
+const { authJwt } = require('../middlewares')
+const router = Router()
+
+router
+    
+    .get('/mentor/:id',authJwt.verifyToken, MentorController.searchMentorById)
+    .get('/mentors', authJwt.verifyToken, authJwt.isMentor, MentorController.searchMentorRandom)
+    .put('/mentor/update/:id', authJwt.verifyToken, authJwt.isMentor,   MentorController.updateMentor)
+    .delete('/mentor/:id',authJwt.verifyToken, authJwt.isMentor,  MentorController.deleteMentor)
+
+module.exports = router
