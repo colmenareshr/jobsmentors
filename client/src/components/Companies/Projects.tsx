605580ac4f17f136b640e00edb402840acca2356
José Freites <jlfu2004@gmail.com>
2023-05-19 14:42:52 -0300

    cleaning ccs code

--- client/src/components/Companies/Projects.tsx
+++ client/src/components/Companies/Projects.tsx
@@ -1,99 +1,116 @@
 import React from 'react'
+import './projects.css'
 
 function Projects() {
   return (
-    <div className="container mx-auto bg-emerald">
-      <section className="bg-teal400/40">
-        <form className=" bg-black/20 p-4">
-          <main
-            className="bg-gray-300 grid h-[900px] grid-flow-col-dense grid-cols-12 grid-rows-6
-                      gap-4 border-2 border-white p-4 text-center
+    <div className="w-full bg-teal400 p-4">
+      <div className="container mx-auto rounded-lg border-2 border-white bg-teal400">
+        <section className="">
+          <form className="pb-4 pl-4 pr-4">
+            <main
+              className="bg-gray-300 grid h-[900px] grid-flow-col-dense grid-cols-12 grid-rows-6
+                      gap-4 rounded-lg text-center
                       "
-          >
-            <header className="col-span-12 row-span-1 flex flex-wrap  content-center justify-center">
-              <p className="p-title-Projects text-center text-5xl font-black">
-                INGRESE LOS DATOS DE SU PROYECTO Y BUSQUE FREELANCERS
-              </p>
-            </header>
+            >
+              <header className="col-span-12 row-span-1 flex w-full flex-wrap content-center justify-center border-b-2 border-white">
+                <p className="p-title-Projects text-center text-5xl font-black text-white drop-shadow-lg">
+                  INGRESE LOS DATOS DE SU PROYECTO Y BUSQUE FREELANCERS
+                </p>
+              </header>
 
-            <div className="col-span-4 row-span-1 self-center">
-              <label htmlFor="" className="text-lg">
-                Nombre del Proyecto
-              </label>
-              <input
-                className="w-full rounded-lg p-3"
-                placeholder="nombre del proyecto"
-                type="text"
-              />
-            </div>
-            <div className="col-span-4 row-span-1 self-center rounded-lg">
-              <label htmlFor="" className="text-lg">
-                Habilidades requeridas
-              </label>
-              <input
-                className="rouded-lg w-full rounded-lg p-3"
-                placeholder="Javascript, React, Node, etc."
-                type="text"
-              />
-            </div>
-            <div className="col-span-4 row-span-1 self-center">
-              <label htmlFor="" className="text-lg">
-                Número de frelancer para el proyecto
-              </label>
-              <input
-                className="rouded-lg w-full rounded-lg p-3"
-                placeholder="min ( 1 ) . . . max ( 20 )"
-                type="text"
-              />
-            </div>
-            <div className="col-span-4 row-span-3 rounded-lg p-4">
-              <label
-                htmlFor=""
-                className="flex content-center justify-center pb-4 text-center text-lg"
-              >
-                Descripción
-              </label>
-              <textarea
-                className="rouded-lg h-3/4 w-full scroll-smooth rounded-lg p-3"
-                placeholder="descripción del proyecto"
-              />
-            </div>
-            <div className="col-span-4 row-span-3 flex flex-col place-content-center gap-16">
-              <div className="">
-                <button className="w-80 rounded-full bg-yellow p-5 text-lg font-semibold shadow-lg">
-                  GUARDAR
-                </button>
+              <div className="col-span-4 row-span-1 self-center">
+                <label htmlFor="" className="label-Projects text-xl">
+                  Nombre del Proyecto
+                </label>
+                <input
+                  className="w-full rounded-lg p-3"
+                  placeholder="nombre del proyecto"
+                  type="text"
+                />
               </div>
-              <div className="">
-                <button className="rounded-full bg-yellow p-5 pl-20 pr-20 text-lg font-semibold shadow-lg">
-                  ELIMINAR PROYECTO
-                </button>
+              <div className="col-span-4 row-span-1 self-center rounded-lg">
+                <label htmlFor="" className="label-Projects text-xl">
+                  Habilidades requeridas
+                </label>
+                <input
+                  className="rouded-lg w-full rounded-lg p-3"
+                  placeholder="Javascript, React, Node, etc."
+                  type="text"
+                />
               </div>
+              <div className="col-span-4 row-span-1 self-center">
+                <label htmlFor="" className="label-Projects text-xl">
+                  Freelancers
+                </label>
+                <input
+                  className="rouded-lg w-full rounded-lg p-3"
+                  placeholder="min ( 1 ) . . . max ( 20 )"
+                  type="text"
+                />
+              </div>
+              <div className="col-span-4 row-span-3 rounded-lg pl-20">
+                <label
+                  htmlFor=""
+                  className="label-Projects flex content-center justify-center pt-7 text-center text-xl"
+                >
+                  Descripción
+                </label>
+                <textarea
+                  className="rouded-lg h-3/4 w-full scroll-smooth rounded-lg p-3"
+                  placeholder="descripción del proyecto"
+                />
+              </div>
+              <div className="col-span-4 row-span-3 flex flex-col place-content-center gap-16">
+                <div className="">
+                  <button className="button w-80 rounded-full p-5 shadow-lg">
+                    GUARDAR
+                  </button>
+                </div>
+                <div className="">
+                  <button className="button rounded-full p-5 pl-20 pr-20 shadow-lg">
+                    ELIMINAR PROYECTO
+                  </button>
+                </div>
+              </div>
+              <div className="col-span-12 row-span-2 flex w-full items-center justify-center rounded-lg bg-emerald">
+                TABLA
+              </div>
+            </main>
+          </form>
+        </section>
+        <section className="p-4">
+          <div className="rounded-lg bg-sky p-4">
+            <div className="flex w-full flex-wrap justify-center p-4">
+              <button className="button flex rounded-full p-5 pl-20 pr-20 shadow-lg">
+                BUSCAR FREELANCERS
+              </button>
             </div>
-            <div className="col-span-12 row-span-2 flex w-full items-center justify-center bg-teal400/40">
-              TABLA
+            <div className="flex flex-wrap justify-evenly gap-5 p-4">
+              <div className="h-44 w-96 rounded-lg bg-white">CARD</div>
+              <div className="h-44 w-96 rounded-lg bg-white">CARD</div>
+              <div className="h-44 w-96 rounded-lg bg-white">CARD</div>
+              <div className="h-44 w-96 rounded-lg bg-white">CARD</div>
+              <div className="h-44 w-96 rounded-lg bg-white">CARD</div>
+              <div className="h-44 w-96 rounded-lg bg-white">CARD</div>
+              <div className="h-44 w-96 rounded-lg bg-white">CARD</div>
+              <div className="h-44 w-96 rounded-lg bg-white">CARD</div>
+              <div className="h-44 w-96 rounded-lg bg-white">CARD</div>
             </div>
-          </main>
-        </form>
-      </section>
-      <section>
-        <div className="flex w-full flex-wrap justify-center p-4">
-          <button className="flex rounded-full bg-yellow p-5 pl-20 pr-20 font-semibold shadow-lg">
-            BUSCAR FREELANCERS
-          </button>
-        </div>
-        <div className="flex flex-wrap justify-evenly gap-5 p-4">
-          <div className="h-44 w-96 bg-white">CARD</div>
-          <div className="h-44 w-96 bg-white">CARD</div>
-          <div className="h-44 w-96 bg-white">CARD</div>
-          <div className="h-44 w-96 bg-white">CARD</div>
-          <div className="h-44 w-96 bg-white">CARD</div>
-          <div className="h-44 w-96 bg-white">CARD</div>
-          <div className="h-44 w-96 bg-white">CARD</div>
-          <div className="h-44 w-96 bg-white">CARD</div>
-          <div className="h-44 w-96 bg-white">CARD</div>
-        </div>
-      </section>
+          </div>
+          <div className="flex flex-row items-center justify-evenly gap-16 pb-6 pt-10">
+            <div className="">
+              <button className="button p-5 pl-24 pr-24 shadow-lg">
+                CANCELAR
+              </button>
+            </div>
+            <div className="">
+              <button className="button p-5 pl-10 pr-10 shadow-lg">
+                AGREGAR FREELANCERS
+              </button>
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   )
 }
