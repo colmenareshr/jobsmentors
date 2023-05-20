6af9d9b67d967172d0d82b1f8a304a12470982f1
José Freites <jlfu2004@gmail.com>
2023-05-19 22:24:47 -0300

    ADDED fetch in context

--- client/src/context/useStore.tsx
+++ client/src/context/useStore.tsx
@@ -1,4 +1,7 @@
+import { use } from 'chai'
+import { get } from 'http'
 import { useEffect, useState } from 'react'
+// import { useLocation } from 'react-router-dom'
 
 export type ProjectsProps = {
   id: number
@@ -8,10 +11,43 @@ export type ProjectsProps = {
   availability: number
 }
 
+export type CandidateProps = {
+  id: number
+  name: string
+  email: string
+  password: string
+  skills: string[]
+  img: string
+}
+
 export const useStore = () => {
   const [isOpenModalLogin, setIsOpenModalLogin] = useState(false)
   const [isOpenModalSign, setIsOpenModalSign] = useState(false)
   const [searchTerm, setSearchTerm] = useState('')
+  const [items, setItems] = useState<CandidateProps[]>([])
+  const location = window.location.pathname
+
+  const isCantidate = location.includes('/candidate')
+  const isCompany = location.includes('/company')
+  let pathRoute = ''
+
+  const getItems = async () => {
+    if (isCantidate) {
+      let pathRoute = 'candidate'
+    } else {
+      let pathRoute = 'company'
+    }
+
+    const response = await fetch('http://localhost:4000/' + `${pathRoute}`)
+    const data = await response.json()
+
+    setItems(data)
+    // setFilteredItems(data)
+  }
+
+  useEffect(() => {
+    getItems()
+  }, [])
 
   return {
     isOpenModalLogin,
@@ -19,6 +55,8 @@ export const useStore = () => {
     isOpenModalSign,
     setIsOpenModalSign,
     searchTerm,
-    setSearchTerm
+    setSearchTerm,
+    location,
+    items
   }
 }
