72054b3b8af9d820f777c49c7857c99de66f860a
Humberto Colmenares <colmenareshr@gmail.com>
2023-05-20 06:04:55 -0300

    traslate section

--- client/src/components/BigProjectSection/BigProjectSection.tsx
+++ client/src/components/BigProjectSection/BigProjectSection.tsx
@@ -3,17 +3,20 @@ import CompanyImages from 'components/CompanyImages/CompanyImages'
 const BigProjectSection = () => {
   return (
     <section className="container mx-auto max-w-full bg-[#fff] py-14 text-center">
-      <h2>Looking for a Development Team for Large-Scale Projects?</h2>
+      <h2>
+        Procurando uma equipe de desenvolvimento para projetos de grande escala?
+      </h2>
       <div className="flex flex-col-reverse gap-4 py-5 md:flex-row md:items-center md:justify-center md:px-5">
         <div className="md:w-[50%] md:text-left">
-          <h3>Unlock the Potential of Your Projects</h3>
+          <h3>Desbloqueie o potencial dos seus projetos</h3>
           <p className="pb-4">
-            Build your dream team of skilled developers and elevate your
-            large-scale projects to new heights. Our platform connects you with
-            expert mentors to ensure success every step of the way.
+            Construa sua equipe dos sonhos de desenvolvedores habilidosos e
+            eleve seus projetos de grande escala a novas alturas. Nossa
+            plataforma conecta você a mentores especializados para garantir o
+            sucesso em cada etapa do caminho.
           </p>
           <a href="#" className="button">
-            Create your powerhouse team now
+            Crie sua equipe de sucesso agora
           </a>
         </div>
         <div>
