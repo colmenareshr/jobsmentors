52172030685b342a9c7d8ffa0ad1fd0ae47785b2
Pedro Bonett <gilbonett@gmail.com>
2023-05-20 15:29:51 -0300

    importing mentor route

--- server/app/routes/index.js
+++ server/app/routes/index.js
@@ -2,12 +2,14 @@ const bodyParser = require ('body-parser')
 const auth = require('./authRoute')
 const freelancer = require('./freelancerRoute')
 const company = require('./companyRoute')
+const mentor = require('./mentorRoute')
 
 module.exports = app => {
     app.use(
         bodyParser.json(),
         freelancer,
         company,
+        mentor,
         auth
     )
 }
