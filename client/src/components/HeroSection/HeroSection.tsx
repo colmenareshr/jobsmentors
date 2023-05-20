932732b204fbab393864392912f3f2c117b00c5d
Humberto Colmenares <colmenareshr@gmail.com>
2023-05-20 05:46:03 -0300

    traslate herosection

--- client/src/components/HeroSection/HeroSection.tsx
+++ client/src/components/HeroSection/HeroSection.tsx
@@ -2,12 +2,12 @@ const HeroSection = () => {
   return (
     <section className="flex h-[50vh] w-full flex-col items-center justify-center gap-4 text-center ">
       <div className="container-lg mx-auto">
-        <h1 className="max-w-xl md:text-6xl">
-          Find the perfect freelance talent for your technological projects.
+        <h1 className="max-w-4xl md:text-6xl">
+          Encontre o talento freelance perfeito para seus projetos tecnológicos.
         </h1>
       </div>
       <a href="#" className="button uppercase">
-        Get a solution now
+        Obtenha uma solução agora
       </a>
     </section>
   )
