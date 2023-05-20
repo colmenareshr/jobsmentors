29881971770dcca111e32198c361e67f65780c08
José Freites <jlfu2004@gmail.com>
2023-05-20 11:55:28 -0300

    MOD var nome to name || export freelancer data

--- client/src/components/FreelancerCard2/FreelamcerCard2.tsx
+++ client/src/components/FreelancerCard2/FreelamcerCard2.tsx
@@ -1,21 +1,21 @@
 interface infoCard {
   image: string
-  nome: string
+  name: string
   skill: string
 }
 
-function FreelancerCard2({ image, nome, skill }: infoCard) {
+function FreelancerCard2({ image, name, skill }: infoCard) {
   return (
     <div className="flex h-[200px] w-full max-w-[400px] flex-nowrap items-center justify-center gap-3 rounded-md bg-white">
       <div className="">
         <img
           src={`https://images.unsplash.com/photo-${image}`}
-          alt={nome}
+          alt={name}
           className="h-[100px] w-[100px] rounded-full object-cover object-center "
         />
       </div>
       <div className="flex max-w-[200px] flex-col flex-wrap text-left ">
-        <h4 className="text-[20px] font-bold ">{nome}</h4>
+        <h4 className="text-[20px] font-bold ">{name}</h4>
         <p className=" font-semibold">{skill}</p>
       </div>
     </div>
