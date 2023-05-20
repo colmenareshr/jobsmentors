2c3f00b2dc85989015ed01695892bf11e9b45607
Humberto Colmenares <colmenareshr@gmail.com>
2023-05-20 06:17:46 -0300

    add lazy loadign to img hero section

--- client/src/components/CtaJuniors/CtaJuniors.tsx
+++ client/src/components/CtaJuniors/CtaJuniors.tsx
@@ -4,7 +4,12 @@ const CtaJuniors = () => {
   return (
     <section className="relative h-full w-full bg-emerald py-16 text-center">
       <div className="absolute top-[-20%] md:absolute md:-top-1/3  md:left-1/2 md:-translate-x-1/2 ">
-        <img className="z-9" src={Freelancer} alt="Freelancers Junior" />
+        <img
+          className="z-9"
+          src={Freelancer}
+          alt="Freelancers Junior"
+          loading="lazy"
+        />
       </div>
       <div className="container-lg mx-auto mt-24 grid md:mt-0 md:grid md:grid-cols-2 md:items-center md:justify-items-center md:px-9">
         <div className="flex flex-col items-center gap-3 text-center text-white md:w-[80%] md:items-start md:text-left">
