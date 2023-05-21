ad7decfb483d5780289be126f587dad1fbad741d
José Freites <jlfu2004@gmail.com>
2023-05-21 13:35:09 -0300

    DEL non use code

--- client/src/context/appContext.tsx
+++ client/src/context/appContext.tsx
@@ -1,7 +1,7 @@
 import { createContext } from 'react'
 import { Candidate, Company } from './useStore'
 import { User } from 'interfaces/AuthInterfaces'
-import { currentUser, setCurrenUser } from './authContext'
+// import { currentUser, setCurrenUser } from './authContext'
 
 export interface AppContextProps {
   isOpenModalLogin: boolean
@@ -14,8 +14,8 @@ export interface AppContextProps {
   setCandidate: (candidate: Candidate[]) => void
   company: Company[]
   setCompany: (company: Company[]) => void
-  currentUser: User[]
-  setCurrentUser: (currentUser: User[]) => void
+  // currentUser: User[]
+  // setCurrentUser: (currentUser: User[]) => void
 }
 
 export const AppContext = createContext<AppContextProps | null>(null)
