420c39dbc49cfbfb388e859a868d1043c88bc036
Humberto Colmenares <colmenareshr@gmail.com>
2023-05-21 06:01:19 -0300

    apply authcontext

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,8 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import 'tailwindcss/tailwind.css'
 import App from './App'
+import { AuthContextProvider } from 'context/authContext.tsx'
 
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
 
-root.render(<App />)
+root.render(
+  <AuthContextProvider>
+    <App />
+  </AuthContextProvider>
+)
