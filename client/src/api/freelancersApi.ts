d58c2f17f87cad7cb0d348c6175932498cbcac0e
Humberto Colmenares <colmenareshr@gmail.com>
2023-05-21 19:24:52 -0300

    adding api methods to freelancers

---
+++ client/src/api/freelancersApi.ts
@@ -0,0 +1,79 @@
+import { AxiosResponse } from 'axios'
+import api from 'api'
+
+interface FreelancerData {
+  id: number
+  email: string
+  password: string
+  role: 'freelancer' | 'company' | 'mentor'
+}
+
+export const getFreelancers = async (): Promise<
+  AxiosResponse<FreelancerData[]>
+> => {
+  try {
+    const res = await api.get<FreelancerData[]>('/freelancers')
+    return res
+  } catch (error) {
+    console.error('Error getting freelancers:', error)
+    throw error
+  }
+}
+
+export const createFreelancer = async (
+  freelancerData: FreelancerData
+): Promise<AxiosResponse<any>> => {
+  try {
+    const res = await api.post('/freelancers', freelancerData)
+    return res
+  } catch (error) {
+    console.error('Error creating freelancer:', error)
+    throw error
+  }
+}
+
+export const getFreelancerById = async (
+  id: string
+): Promise<AxiosResponse<FreelancerData>> => {
+  try {
+    const res = await api.get<FreelancerData>(`/freelancers/${id}`)
+    return res
+  } catch (error) {
+    console.error(`Error getting freelancer with ID ${id}:`, error)
+    throw error
+  }
+}
+
+interface FreelancerUpdateData {
+  email?: string
+  password?: string
+  role?: 'freelancer' | 'company' | 'mentor'
+}
+
+export const updateFreelancer = async (
+  id: string,
+  freelancerData: FreelancerUpdateData
+): Promise<AxiosResponse<FreelancerData>> => {
+  try {
+    const res = await api.put<FreelancerData>(
+      `/freelancers/${id}`,
+      freelancerData
+    )
+    return res
+  } catch (error) {
+    console.error(`Error updating freelancer with ID ${id}:`, error)
+    throw error
+  }
+}
+
+export const deleteFreelancer = async (
+  id: string
+): Promise<AxiosResponse<void>> => {
+  try {
+    const res = await api.delete<void>(`/freelancers/${id}`)
+    return res
+  } catch (error) {
+    console.error(`Error deleting freelancer with ID ${id}:`, error)
+    throw error
+  }
+}
