23fe10d331bcee0bbc8d58f38c99d5cbccfd2a9c
Pedro Bonett <gilbonett@gmail.com>
2023-05-22 13:10:42 -0300

    correcting company route

--- server/app/routes/companyRoute.js
+++ server/app/routes/companyRoute.js
@@ -6,18 +6,18 @@ const router = Router()
 
 router
     
-    .get('/company/:id', authJwt.verifyToken, CompanyController.searchCompanyById)
+    .get('/company/:user_id', authJwt.verifyToken, CompanyController.searchCompanyById)
     .get('/companies', CompanyController.searchCompanies)
     .get('/companies/rand', authJwt.verifyToken, CompanyController.searchCompaniesRandom)
-    .get('/company/:id/jobs',  CompanyController.searchJobsCompany)
-    .put('/company/update/:id', authJwt.verifyToken, authJwt.isCompany,  CompanyController.updateCompany)
-    .delete('/company/:id', authJwt.verifyToken, authJwt.isCompany, CompanyController.deleteCompany)
+    .get('/company/:user_id/jobs',  CompanyController.searchJobsCompany)
+    .put('/company/:user_id', authJwt.verifyToken, authJwt.isCompany,  CompanyController.updateCompany)
+    .delete('/company/:user_id', authJwt.verifyToken, authJwt.isCompany, CompanyController.deleteCompany)
 
-    .post('/company/:id/job', authJwt.verifyToken, authJwt.isCompany, CompanyController.CreateJob)
+    .post('/company/:user_id/job', authJwt.verifyToken, authJwt.isCompany, CompanyController.CreateJob)
     .get('/companies/jobs', authJwt.verifyToken, CompanyController.searchJobsCompanies)
     .get('/company/:id/freelancers', authJwt.verifyToken, CompanyController.FreelancerAtJobs)
-    .put('/company/job/:id',authJwt.verifyToken, authJwt.isCompany, CompanyController.updateJob)
-    .delete('/company/job/:id',authJwt.verifyToken, authJwt.isCompany, CompanyController.deleteJob)
+    .put('/company/:user_id/:id',authJwt.verifyToken, authJwt.isCompany, CompanyController.updateJob)
+    .delete('/company/:user_id/:id',authJwt.verifyToken, authJwt.isCompany, CompanyController.deleteJob)
     .get('/company/:id/matching',authJwt.verifyToken, authJwt.isCompany,  CompanyController.MatchWithSkills)
     .get('/company/:id/freelancerSkills',authJwt.verifyToken, authJwt.isCompany, CompanyController.findFreelancerSkills)
     
