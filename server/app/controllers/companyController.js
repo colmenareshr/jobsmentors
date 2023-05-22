a796a523ff63d08b6ea0f86f981b106b85402425
Pedro Bonett <gilbonett@gmail.com>
2023-05-22 13:10:14 -0300

    correcting company controller

--- server/app/controllers/companyController.js
+++ server/app/controllers/companyController.js
@@ -1,17 +1,16 @@
 const database = require('../models')
 const Sequelize = require('sequelize');
-const Information = require('../models/Information');
 
 
 
 class CompanyController {
 
     static async searchCompanyById(req, res){
-        const {id} = req.params
+        const id = req.params.user_id
         try {
             const resultCompany = await database.Company.findOne({
                 where: {
-                    id: Number(id)
+                    user_id: Number(id)
                 }
             })
             if(resultCompany !== null){
@@ -38,7 +37,7 @@ class CompanyController {
     }
 
     static async searchJobsCompany(req, res){
-        const {id} = req.params
+        const id = req.params.user_id
         try {
             const resultJobs = await database.Jobs.findAll({
                 where: {company_id : Number(id)}
@@ -70,15 +69,19 @@ class CompanyController {
     }
 
     static async updateCompany(req, res) {
-        const uptadedCompany = req.body
-        const {id} = req.params
+        const {
+            name, 
+            bio,
+            site
+        } = req.body
+        const id = req.params.user_id
         try {
             const resultCompany = await database.Company.findOne({
-                where: {id: Number(id)}
+                where: {user_id: id }
             })
             if(resultCompany !== null){
-            await database.Company.update(uptadedCompany, {where: {id:Number(id)}})
-            const companyUpdated = await database.Company.findOne({where: {id:Number(id)}})
+            await database.Company.update({name, site, bio}, {where: {user_id:Number(id)}})
+            const companyUpdated = await database.Company.findOne({where: {user_id:Number(id)}})
             return res.status(200).json(companyUpdated)
             } else {
                 return res.status(400).send({message:`Company ${id} not found`})
@@ -89,14 +92,14 @@ class CompanyController {
     }
 
     static async deleteCompany(req, res) {
-        const {id}= req.params
+        const id = req.params.user_id
         try {
             const resultCompany = await database.Company.findOne({
-                where: {id: Number(id)}
+                where: {user_id: Number(id)}
             })
             if(resultCompany !== null){
-                await database.Company.destroy({where: {id : Number(id)}})
-                return res.status(200).send({message: `successfully deleted Company ${id} `})
+                await database.Company.destroy({where: {user_id : Number(id)}})
+                return res.status(200).send({message: `successfully deleted Company ${resultCompany.name} `})
             } else {
                 return res.status(400).send({message:'Company id not found'})
             }
@@ -106,17 +109,24 @@ class CompanyController {
     }
 
     static async CreateJob(req, res) {
-        const {id}= req.params
-        const {company_id, title, description, hard_skills} = req.body 
+        const id = req.params.user_id
         try {
-            const newJob = await database.Jobs.create({   
-                company_id: id,
-                title, 
-                description,
-                hard_skills: hard_skills.toLowerCase()}
-            )
-
-            return res.status(200).json(newJob)
+            const resultCompany = await database.Company.findOne({
+                where: {user_id: Number(id)}
+            })
+            if(resultCompany !== null){
+                const {company_id, title, description, hard_skills} = req.body 
+                const newJob = await database.Jobs.create({   
+                    company_id: id,
+                    title, 
+                    description,
+                    hard_skills: hard_skills.toLowerCase()}
+                )
+    
+                return res.status(200).json(newJob)
+            } else {
+                return res.status(400).send({message:`Company ${id} not found`})
+            }
         } catch (error) {
             return res.status(500).json(error.message)
         }
@@ -150,15 +160,23 @@ class CompanyController {
 
     static async updateJob(req, res) {
         const {title, description, hard_skills} = req.body 
-        const {id} = req.params
+        const id = req.params.user_id
+        const JobId = req.params.id
         try {
-            const resultJob = await database.Jobs.findByPk(id)
-            if(resultJob !== null){
-            await database.Jobs.update({title, description, hard_skills:hard_skills.toLowerCase()}, {where: {id:Number(id)}})
-            const jobUpdated = await database.Jobs.findOne({where: {id:Number(id)}})
-            return res.status(200).json(jobUpdated)
+            const resultCompany = await database.Company.findOne({
+                where: {user_id:Number(id) }
+            })
+            if(resultCompany !== null){
+                const resultJob = await database.Jobs.findOne({
+                    where: {id: Number(JobId)  }
+                })
+                if(resultJob !== null){
+                await database.Jobs.update({title, description, hard_skills:hard_skills.toLowerCase()}, {where: {id:Number(resultJob.id)}})
+                const jobUpdated = await database.Jobs.findOne({where: {id:Number(JobId)}})
+                return res.status(200).json(jobUpdated)
+            }
             } else {
-                return res.status(400).send({message:`Job ${id} not found`})
+                return res.status(400).send({message:`Job ${JobId} not found`})
             }
         } catch (error) {
             return res.status(500).json(error.message)
@@ -166,14 +184,22 @@ class CompanyController {
     }
 
     static async deleteJob(req, res) {
-        const {id}= req.params
+        const id = req.params.user_id
+        const JobId = req.params.id
         try {
-            const resultJob = await database.Jobs.findByPk(id)
-            if(resultJob !== null){
-                await database.Jobs.destroy({where: {id : Number(id)}})
-                return res.status(200).send({message: `successfully deleted job ${id} `})
+            const resultCompany = await database.Company.findOne({
+                where: {user_id:Number(id) }
+            })
+            if(resultCompany !== null){
+                const resultJob = await database.Jobs.findOne({
+                    where: {id: Number(JobId)  }
+                })
+                if(resultJob !== null){
+                await database.Jobs.destroy({where: {id : Number(resultJob.id)}})
+                return res.status(200).send({message: `successfully deleted job ${resultJob.id} `})
+            }
             } else {
-                return res.status(400).send({message:'Job id not found'})
+                return res.status(400).send({message:`Job ${JobId} not found`})
             }
         } catch (error) {
             return res.status(500).json(error.message)
