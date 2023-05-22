25910ecdbb463e7c6b457d8eb94428ed8d58583a
José Freites <jlfu2004@gmail.com>
2023-05-22 16:44:06 -0300

    ADDED code to works with fake data card project

--- client/src/components/Companies/ProjectCard.tsx
+++ client/src/components/Companies/ProjectCard.tsx
@@ -1,14 +1,22 @@
+// This component shows a project card with its details in two differentes modals
+// Frist one, used as crud for the project
+// Second one, used to show the project details
+
 import React, { FC, useState, useContext } from 'react'
 import { useStore, Project } from '../../context/useStore'
 import { randomInt } from 'crypto'
+import { FakeCard } from './Companies'
 
-interface CardProps {
-  project: Project
-}
+// interface CardProps {
+//   project: Project
+// }
 
-function ProjectCard({ project }: CardProps) {
+// function ProjectCard({ project }: CardProps) {
+
+function ProjectCard({ project }: FakeCard) {
   const [isOpenModalProjectCard, setIsOpenModalProjectCard] = useState(true)
 
+  // Showing project details in a modal
   const ModalProjects = () => {
     return (
       <div className="fixed inset-0 z-10 overflow-y-auto">
@@ -19,6 +27,7 @@ function ProjectCard({ project }: CardProps) {
     )
   }
 
+  // Opening Project Card Modal
   const handleOpenModalProjectCard = () => {
     console.log('ProjectCard')
     setIsOpenModalProjectCard(true)
@@ -31,18 +40,18 @@ function ProjectCard({ project }: CardProps) {
     <div
       className="transition-all/300 scrollbar- relative flex w-80 cursor-pointer
         flex-col items-center justify-center rounded-lg border-2 border-white
-        bg-teal400 hover:shadow-lg hover:ring-2 hover:ring-teal400 hover:ring-offset-2"
+        bg-purple/60 hover:shadow-lg hover:ring-2 hover:ring-teal400 hover:ring-offset-2"
       onClick={handleOpenModalProjectCard}
     >
       <header className="w-full border-b-2 pb-1 pt-2 text-center text-2xl font-semibold text-white">
-        {project.name}
+        {project.project}
       </header>
 
       <main className="flex h-52 flex-col">
         <section className="px-2 pt-2 text-center text-white">
           <p>{project.description}</p>
         </section>
-        <section className="h-20 overflow-y-scroll bg-sky pt-2 text-center">
+        <section className="h-20 overflow-y-scroll pt-2 text-center">
           <p className="font-semibold">Habilidades: </p>
           {project.skills}
         </section>
@@ -56,3 +65,29 @@ function ProjectCard({ project }: CardProps) {
 }
 
 export default ProjectCard
+
+// THIS PART WE GOINT TO USED AFTER BACKEND BE DONE
+// <div
+//   className="transition-all/300 scrollbar- relative flex w-80 cursor-pointer
+//     flex-col items-center justify-center rounded-lg border-2 border-white
+//     bg-teal400 hover:shadow-lg hover:ring-2 hover:ring-teal400 hover:ring-offset-2"
+//   onClick={handleOpenModalProjectCard}
+// >
+//   <header className="w-full border-b-2 pb-1 pt-2 text-center text-2xl font-semibold text-white">
+//     {project.name}
+//   </header>
+
+//   <main className="flex h-52 flex-col">
+//     <section className="px-2 pt-2 text-center text-white">
+//       <p>{project.description}</p>
+//     </section>
+//     <section className="h-20 overflow-y-scroll bg-sky pt-2 text-center">
+//       <p className="font-semibold">Habilidades: </p>
+//       {project.skills}
+//     </section>
+//   </main>
+
+//   <footer className="absolute bottom-0 w-full border-t-2 pb-2 pt-2 text-center text-white">
+//     <p className="text-white">Cupos: {availability}/10</p>
+//   </footer>
+// </div>
