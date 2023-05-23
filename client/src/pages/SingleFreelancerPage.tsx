3aa5903cd67738921a0697c98dc81783047de534
Humberto Colmenares <colmenareshr@gmail.com>
2023-05-22 21:34:41 -0300

    Single Freelancer Page

---
+++ client/src/pages/SingleFreelancerPage.tsx
@@ -0,0 +1,87 @@
+import {
+  IoLogoGithub,
+  IoLogoLinkedin,
+  IoLogoReact,
+  IoLogoNodejs,
+  IoLogoWordpress
+} from 'react-icons/io5'
+const SingleFreelancerPage = () => {
+  return (
+    <section className="w-full">
+      <div className="mx-auto -mb-36 max-w-full bg-teal400 py-16"></div>
+      <div className="mx-auto max-w-full pt-16">
+        <div className="container mx-auto bg-white px-3 py-8 text-center">
+          <div className="flex flex-col items-center justify-center">
+            <img
+              className=" h-auto w-[180px] rounded-full"
+              src="https://github.com/colmenareshr.png"
+              alt="Humberto Colmenares"
+            />
+            <h1>Humberto Colmenares</h1>
+          </div>
+          <div className="border-b-1 flex items-center justify-center gap-2 border-b-black/50 py-4">
+            <button className="button-secondary">Editar perfil</button>
+            <button className="text-black">Portfolio</button>
+          </div>
+        </div>
+        <div className="container mx-auto border-t border-t-emerald/30 bg-white p-12">
+          <h3>About me</h3>
+          <p className="text-black/80">
+            I am a person who is passionate about remote work and the area of
+            ​​technology. 🎯 I am currently looking for a job in this sector and
+            be able to contribute my knowledge and experience, both as a full
+            stack web developer, and in the DevOps culture and thus face the
+            challenges that arise in the company. I seek to grow in knowledge,
+            experience and teamwork, in a stable work environment in the area of
+            ​​technology. 👉 I have been working as a freelancer since 2016.
+            Most of my work experience has been as a web designer with WordPress
+            on teleworking platforms. I have also taken on projects individually
+            that come by recommendation. Each and every one has been a great
+            challenge, learning and enjoyable and communicative work. Currently
+            I am still preparing to enter a work environment as a Full Stack and
+            DevOps web developer in various online training courses at different
+            institutions that offer technology training. 🙌
+          </p>
+          <div className="flex items-center gap-3 pt-2">
+            <IoLogoGithub size={25} />
+            <IoLogoLinkedin size={25} />
+          </div>
+        </div>
+        <div className="container mx-auto border-t border-t-emerald/30 bg-white p-12">
+          <h3>Expertise</h3>
+          <div className="flex items-center gap-2">
+            <IoLogoReact />
+            <span>React</span>
+          </div>
+          <div className="flex items-center gap-2">
+            <IoLogoWordpress />
+            <span>WordPress</span>
+          </div>
+          <div className="flex items-center gap-2">
+            <IoLogoNodejs />
+            <span>Node</span>
+          </div>
+        </div>
+        <div className="container mx-auto border-t border-t-emerald/30 bg-white p-12">
+          <h3>Projects</h3>
+          <div>
+            <div className="flex items-center justify-between ">
+              <h4>JobsMentors</h4>
+              <button className="button-secondary">View Projects</button>
+            </div>
+            <span>2023</span>
+            <p>
+              CRUD interface The proposal of the work is, from the theme drawn
+              for the group, to develop an application in React, integrated to a
+              REST...
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="p-12">
+        <button className="button">Delete profile</button>
+      </div>
+    </section>
+  )
+}
+export default SingleFreelancerPage
--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,19 +1,22 @@
 import FreelancersPage from 'pages/Freelancers'
 import Home from 'pages/Home'
 import { Routes, Route, Navigate } from 'react-router-dom'
-// import Companies from '../components/Companies/Companies'
 import CompanyLandingPage from 'components/Companies/CompanyLandingPage'
 import Projects from 'components/Projects/Projects'
+import SingleFreelancerPage from 'pages/SingleFreelancerPage'
+import Companies from 'components/Companies/Companies'
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/freelancers" element={<FreelancersPage />} />
-      <Route path="*" element={<Navigate to="/" />} />
-      {/* <Route path="/company" element={<Companies />} /> */}
+      {/* <Route path="/freelancers/single" element={<SingleFreelancerPage />} /> */}
+      {/* <Route path="/freelancers" element={<FreelancersPage />} /> */}
+      <Route path="/freelancers" element={<SingleFreelancerPage />} />
+      <Route path="/company" element={<Companies />} />
       <Route path="/company/landingpage" element={<CompanyLandingPage />} />
       <Route path="/company/projects" element={<Projects />} />
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   )
 }
