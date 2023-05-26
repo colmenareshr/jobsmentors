57b78ac8b60053ec65d5ca07b5a4b3ef823f16f0
Humberto Colmenares <colmenareshr@gmail.com>
2023-05-26 02:13:39 -0300

    clean code ande conditional render

--- client/src/components/Login/Login.tsx
+++ client/src/components/Login/Login.tsx
@@ -9,21 +9,31 @@ import { useTranslation } from 'react-i18next'
 function Login() {
   const { t } = useTranslation()
   const { currentUser } = useContext(AuthContext) as AuthContextProps
-  const navigate = useNavigate()
   const { isOpenModalLogin, setIsOpenModalLogin } = useContext(
     AppContext
   ) as AppContextProps
+  const navigate = useNavigate()
 
   const handleOpen = () => {
     setIsOpenModalLogin(true)
   }
 
+  const handleLogout = () => {
+    setIsOpenModalLogin(false)
+    navigate('/')
+  }
   return (
     <Fragment>
       <div>
-        <button className="button-secondary" onClick={() => handleOpen()}>
-          {t('app.menu.login')}
-        </button>
+        {currentUser ? (
+          <button className="button-secondary" onClick={handleLogout}>
+            {t('app.menu.logout')}
+          </button>
+        ) : (
+          <button className="button-secondary" onClick={handleOpen}>
+            {t('app.menu.login')}
+          </button>
+        )}
         {isOpenModalLogin && (
           <div className="modal">
             <div className="modal-content">
