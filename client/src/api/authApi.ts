dc80896f8411aa8d8c2ec03a0998605ce68e54c3
Humberto Colmenares <colmenareshr@gmail.com>
2023-05-26 00:31:58 -0300

    Api auth for register user

---
+++ client/src/api/authApi.ts
@@ -0,0 +1,16 @@
+import api from 'api'
+
+interface userData {
+  id: number
+  email: string
+  password: string
+  role: string
+}
+export const registerUser = async (userData: userData) => {
+  try {
+    const response = await api.post('/register', userData)
+    return response.data
+  } catch (error: any) {
+    throw error.response.data
+  }
+}
