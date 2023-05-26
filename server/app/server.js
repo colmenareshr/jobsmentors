1a1ee5f2b82ed2ab907ce9d17b903cdfcfe623e6
José Freites <jlfu2004@gmail.com>
2023-05-26 17:09:38 -0300

    rreseting databases

--- server/app/server.js
+++ server/app/server.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const { sequelize } = require('./models');
-const port = process.env.port  || 3000;
+const express = require("express");
+const { sequelize } = require("./models");
+const port = process.env.port || 3000;
 const app = express();
-const routes = require('./routes')
-const cors = require('cors');
+const routes = require("./routes");
+const cors = require("cors");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-routes(app)
+routes(app);
 
 app.listen(port, () => {
-    sequelize.authenticate().then(() => {
-        console.log('DB connection successfull')
-    })
+  sequelize.authenticate().then(() => {
+    console.log("DB connection successfull");
+  });
 
-    sequelize.sync({force:false}).then (() => {
-        console.log(`Connecting port ${port}`)
-    }) 
-})
+  sequelize.sync({ force: false }).then(() => {
+    console.log(`Connecting port ${port}`);
+  });
+});
