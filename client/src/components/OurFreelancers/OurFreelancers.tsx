2d69a5e73b74bef9c7c4c4261bc60f6a6007e518
José Freites <jlfu2004@gmail.com>
2023-05-28 20:32:57 -0300

    OurFreelancers Component translation

--- client/src/components/OurFreelancers/OurFreelancers.tsx
+++ client/src/components/OurFreelancers/OurFreelancers.tsx
@@ -1,18 +1,18 @@
 import FreelancerCard from 'components/FreelancerCard/FreelancerCard'
+import { useTranslation } from 'react-i18next'
 
 const OurFreelancers = () => {
+  const { t } = useTranslation()
   return (
     <section className="continer mx-auto max-w-full bg-emerald py-14 text-center">
-      <h2 className="text-white">Conheça nossos talentosos freelancers</h2>
+      <h2 className="text-white">{t('app.ourfreelancers.title')}</h2>
       <div className="flex items-center justify-center gap-4 py-12">
         <FreelancerCard />
       </div>
       <div>
-        <h3 className="pb-4 text-white">
-          Capacite seus projetos com freelancers excepcionais
-        </h3>
+        <h3 className="pb-4 text-white">{t('app.ourfreelancers.comment')}</h3>
         <a href="" className="button">
-          Contrate um freelancer agora
+          {t('app.ourfreelancers.button')}
         </a>
       </div>
     </section>
