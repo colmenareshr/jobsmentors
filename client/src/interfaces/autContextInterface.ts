d00a06281f6770f8d27ef405e2df2e476b5211f1
Humberto Colmenares <colmenareshr@gmail.com>
2023-05-29 20:17:25 -0300

    Update User interface

--- client/src/interfaces/autContextInterface.ts
+++ client/src/interfaces/autContextInterface.ts
@@ -1,6 +1,7 @@
 import { User } from './AuthInterfaces'
 
 export interface AuthContextProps {
+  token: string | User
   currentUser: User | null
   login: (inputs: { email: string; password: string }) => Promise<void>
   logout: () => void
