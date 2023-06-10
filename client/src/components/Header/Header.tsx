d3df6af9960b4dd735f785d3a6764ffdcca01276
Humberto Colmenares <colmenareshr@gmail.com>
2023-06-10 18:41:59 -0300

    cleaning code

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,24 +1,11 @@
 import Navbar from 'components/Navbar/Navbar'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import JobMentorLogo from '../../public/JobMentors-logo.png'
 
 function Header() {
-  const navigate = useNavigate()
-
-  const handleClick = () => {
-    navigate('/')
-  }
   return (
-    <header
-      className="container fixed top-0 z-50 mx-auto flex h-28 
-                max-w-full items-center justify-around 
-                border-b-2 border-b-sky/50 bg-white 
-                "
-    >
-      <div
-        className="flex  cursor-pointer items-center justify-center"
-        onClick={handleClick}
-      >
+    <header className="container fixed top-0 z-50 mx-auto flex h-28 max-w-full items-center justify-around border-b-2 border-b-sky/50 bg-white">
+      <div className="flex  cursor-pointer items-center justify-center">
         <Link to="/">
           <img
             src={JobMentorLogo}
@@ -26,7 +13,6 @@ function Header() {
             className="h-auto w-[230px]"
           />
         </Link>
-        <span className="px-3 text-lg font-bold md:text-2xl"></span>
       </div>
       <Navbar />
     </header>
