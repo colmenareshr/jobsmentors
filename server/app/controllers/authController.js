4283b92875ba9bc5a7ab53d288fffada347e00b6
Pedro Bonett <gilbonett@gmail.com>
2023-06-11 10:31:53 -0300

    applying login by services

--- server/app/controllers/authController.js
+++ server/app/controllers/authController.js
@@ -1,39 +1,28 @@
-const database = require("../models");
-const bcrypt = require("bcrypt");
-const jwt = require("jsonwebtoken");
-const authConfig = require("../../config/authConfig");
+const AuthService = require("../services/authService")
 
 class authController {
-  static async createToken(user) {
-    const payload = {
-      id: user.id,
-      email: user.email,
-      role: user.role,
-    };
-    const token = jwt.sign(payload, authConfig.secret, {
-      expiresIn: authConfig.expires,
-    });
-    return token;
-  }
-
 
   static async logIn(req, res) {
     const { email, password } = req.body;
+
+    if(!email || !password) {
+       return res.status(400).send('bad request')
+    }
+
     try {
-      const user = await database.User.findOne({ where: { email } });
-      if (!user) {
-        return res.status(404).json({ message: "Invalid email or password" });
+      const user = { email, password };
+      const authResult = await AuthService.authenticate(user);
+
+      if (!authResult || !authResult.token) {
+        return res.status(401).send('Invalid email or password');
       }
 
-      if (bcrypt.compareSync(password, user.password)) {
-        const token = await authController.createToken(user);
-        res.status(200).json({ token });
-      }
+      return res.status(200).json({ token: authResult.token });
+      
     } catch (error) {
       return res.status(500).json(error.message);
     }
   }
-
 }
 
 module.exports = authController;
