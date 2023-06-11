a01b1f3695b1d109c5c3b26a81a6be43965ac169
Humberto Colmenares <colmenareshr@gmail.com>
2023-06-10 23:02:08 -0300

    adding responsive

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -30,13 +30,15 @@ const LoginPage = () => {
   }
   return (
     <div className="mt-20 h-[100vh] w-full bg-sky/20  py-16">
-      <div className="container mx-auto flex min-h-[100px] w-full flex-col items-center justify-center gap-4 rounded-xl bg-white p-12 text-center shadow-2xl md:w-1/2 lg:w-1/3">
-        <img
-          src={JobMentorsLogo}
-          alt="JobMentors Logo"
-          className="h-auto w-[250px]"
-        />
-        <span className="text-base font-bold">Inicia tu sesión</span>
+      <div className="container mx-auto flex h-[100%] w-80 flex-col items-center justify-center gap-4 rounded-xl bg-white p-4 text-center shadow-2xl md:w-96">
+        <div className="flex flex-col items-center justify-center pt-8">
+          <img
+            src={JobMentorsLogo}
+            alt="JobMentors Logo"
+            className="h-auto w-[200px]"
+          />
+          <span className="text-base font-bold">Inicia tu sesión</span>
+        </div>
         <div className="container px-5 py-2">
           <form
             onSubmit={handleSubmit}
